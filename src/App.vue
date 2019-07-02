2521b978dab11632347efe662da4e0dfb13fa12b
li-jin2017 <952272325@qq.com>
2019-07-02 19:20:28 +0800

    支持element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+
+    <el-row>
+      <el-button>按钮</el-button>
+    </el-row>
   </div>
 </template>
 
